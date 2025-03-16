2bb49aab2ae8aa6103b9b50d51a47cc9c426127f
MarcosGual <marcos.gual@outlook.com>
2025-03-16 15:28:08 -0300

    add: app components

--- auth.ts
+++ auth.ts
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth"
+import Github from "next-auth/providers/github"
 
-export const authOptions = {
+export const { handlers, auth, signIn, signOut } = NextAuth({
   // Configure one or more authentication providers
-  providers: [],
-}
+  providers: [Github({ clientId: process.env.AUTH_GITHUB_ID || "", clientSecret: process.env.AUTH_SECRET || "" })],
+});
 
-export default NextAuth(authOptions)
+// export default NextAuth(authOptions)
