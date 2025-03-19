fcd4bd0aaa23ad77fcf4ec75a1311d648d27e8d7
MarcosGual <marcos.gual@outlook.com>
2025-03-18 23:49:31 -0300

    add: custom fonts + tailwind config fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = async () => {
 
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
-      <nav className="flex justify-between items-center">
+      <nav className="flex justify-between items-center font-work-sans">
         <Link href="/">
           <Image src="/pslogo.png" alt="logo" width={160} height={40} />
         </Link>
